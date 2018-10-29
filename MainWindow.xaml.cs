c43dfc7c1b34949b3bb53f69495028a61ff5f49d
borreti <36523422+borreti@users.noreply.github.com>
2018-10-29 15:42:42 -0300

    Create MainWindow.xaml.cs
    Criação do arquivo

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Trabalho_Grafos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void box_grafo_selecionado_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //box_grafo_selecionado.Items.Add();
+        }
+
+        private void ler_arquivo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
